deba6efe8863fe5027339ee542e4ffddb7d9fdb2
Joshua Paino <joshua.paino@gmail.com>
2023-01-10 17:10:10 -0600

    Change blog CTA

--- components/BlogCTA.vue
+++ components/BlogCTA.vue
@@ -4,14 +4,14 @@
   <div class="mx-auto max-w-7xl py-12 px-6 rounded-lg sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
     <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
       <span class="block dark:text-gray-200 ">Want to chat?</span>
-      <span class="block text-[#579BB1] dark:text-[#FFF8EA]">Contact me via Email or Linkedin.</span>
+      <span class="block  dark:text-[#FFF8EA]">Contact me via Email or Linkedin.</span>
     </h2>
     <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
       <div class="inline-flex rounded-md shadow">
-        <a href="mailto:joshua.paino@gmail.com" class="inline-flex items-center justify-center rounded-md border border-transparent bg-[#579BB1] px-5 py-3 text-base font-medium dark:bg-[#9E7676] dark:hover:bg-[#815B5B] text-white  hover:bg-[#74b1c4]">Email</a>
+        <a href="mailto:joshua.paino@gmail.com" class="inline-flex items-center justify-center rounded-md border border-transparent px-5 py-3 text-base font-medium dark:bg-[#9E7676] dark:hover:bg-[#815B5B] text-base hover:bg-[#74b1c4]  text-white  bg-[#579BB1] ">Email</a>
       </div>
       <div class="ml-3 inline-flex rounded-md shadow">
-        <a href="" class="inline-flex items-center justify-center rounded-md border border-transparent dark:bg-gray-200 dark:text-gray-800 bg-gray-800 text-white px-5 py-3 text-base font-medium dark:hover:bg-gray-300  hover:bg-gray-700">Linkedin</a>
+        <a href="https://www.linkedin.com/in/joshuapaino" class="inline-flex items-center justify-center rounded-md border border-transparent dark:bg-gray-200 dark:text-gray-800 bg-gray-800 text-white px-5 py-3 text-base font-medium dark:hover:bg-gray-300  hover:bg-gray-700">Linkedin</a>
       </div>
     </div>
   </div>
